2c41ff0a172da5006338b6149f04ce7a110fe092
RobertOu0213 <starshining12345@gmail.com>
2023-11-15 15:55:15 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const routes = require("./routes");
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 app.use(routes);
 
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express();
 
 router.get("/", (req, res) => {
-  res.send(`hello !!!!`);
+  res.render("index");
 });
 
 module.exports = router;
