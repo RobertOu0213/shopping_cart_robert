acac8a7ff446ff60753b5846f78d7fe7cd97e220
RobertOu0213 <starshining12345@gmail.com>
2023-11-16 12:17:30 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const router = express();
+const router = express.Router();
+const productController = require("../controllers/product-controller");
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/products", productController.getProducts);
+
+router.use("/", (req, res) => {
+  res.redirect("/products");
 });
 
 module.exports = router;
