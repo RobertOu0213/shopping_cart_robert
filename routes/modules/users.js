23350540ed53c4a88365ac9a97d9fba66d08d99e
RobertOu0213 <starshining12345@gmail.com>
2023-11-16 15:27:37 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../../controllers/user-controller')
 
 router.get('/login', userController.loginPage)
+router.get('/register', userController.registerPage)
 
 module.exports = router;
