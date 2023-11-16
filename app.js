bf1117475377a63152aded7211e63854d32458b6
RobertOu0213 <starshining12345@gmail.com>
2023-11-17 00:59:55 +0800

    feat: add connect_flash

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const PORT = 3000;
 const routes = require("./routes");
 const exphbs = require("express-handlebars");
 const path = require("path");
+const flash = require("connect-flash");
+const session = require("express-session");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  session({
+    secret: "test",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash("success_messages"); // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash("error_messages"); // 設定 error_msg 訊息
+  res.locals.warning_messages = req.flash("warning_messages"); // 設定 warning_msg 訊息
+  return next();
+});
 app.use(routes);
 
 app.listen(PORT, () => {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -14,24 +14,29 @@ const userController = {
       const user = await User.findOne({ where: { email } });
       const salt = await bcrypt.genSaltSync(10);
       const hash = await bcrypt.hashSync(password, salt); ////密碼加密
-
-      if (!email || !password || confirmPassword) {
+      
+      if (!email || !password || !confirmPassword) {
+        req.flash("error_messages", "全部欄位都需填寫");
         return res.redirect("back");
       }
       if (password !== confirmPassword) {
+        req.flash("error_messages", "密碼不相同");
         return res.redirect("back");
       }
       if (user) {
+        req.flash("error_messages", "帳號註冊過了");
         return res.redirect("back");
       }
+
       await User.create({
         email,
         password: hash,
         role: "user",
       });
+      req.flash("success_messages", "註冊成功");
       return res.redirect("/users/login");
     } catch {
-      (err) => next(err);
+      (err) => console.log(err);
     }
   },
 };
