f07b61bb061770bb9ff9c5a1c5d6fd0f1bb35d7c
RobertOu0213 <starshining12345@gmail.com>
2023-11-16 14:47:45 +0800

    feat: add login page

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express();
 const PORT = 3000;
 const routes = require("./routes");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product-controller");
 const admin = require("./modules/admin");
+const users = require("./modules/users");
 
 router.use("/admin", admin);
+router.use("/users", users);
 router.get("/products", productController.getProducts);
 router.use("/", (req, res) => {
   res.redirect("/products");
