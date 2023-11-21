c7a1dd9226cec0f7cac94a25118e055d97ef2881
RobertOu0213 <starshining12345@gmail.com>
2023-11-21 23:54:20 +0800

    feat: add addCart & subCart controller

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -2,13 +2,24 @@ const { Cart, CartItem } = require('../models')
 
 const cartController = {
   getCart: async (req, res) => {
-    const UserId = req.user.id
-    let cart = await Cart.findOne({ where: { UserId }, include: 'items' })
-    if (!cart) res.render('cart')
-    cart = cart.toJSON()
-    console.log(cart.items)
-    const totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
-    res.render('cart', { cart, totalPrice })
+    try {
+      const user = req.user
+      if (!user) {
+        req.flash('warning_messages', '請先登入!')
+        return res.redirect('/users/login-in')
+      }
+
+      const UserId = req.user.id
+      let cart = await Cart.findOne({ where: { UserId }, include: 'items' })
+      if (!cart) res.render('cart')
+      cart = cart.toJSON()
+      console.log(cart.items)
+      const totalPrice = cart.items.length > 0 ? cart.items.map(d => d.price * d.CartItem.quantity).reduce((a, b) => a + b) : 0
+
+      res.render('cart', { cart, totalPrice })
+    } catch (err) {
+      console.log(err)
+    }
   },
   postCart: async (req, res) => {
     try {
@@ -38,11 +49,26 @@ const cartController = {
       if (!created) product.quantity += 1
       await product.save()
       req.session.cartId = cart.id
-      console.log(req.body)
       return res.redirect('back')
     } catch (err) {
       console.log(err)
     }
+  },
+  addCart: async (req, res) => {
+    try {
+      const id = req.params.id
+      const cartItem = await CartItem.findByPk(id)
+      await cartItem.update({ quantity: cartItem.quantity + 1 })
+      return res.redirect('back')
+    } catch (err) { console.log(err) }
+  },
+  subCart: async (req, res) => {
+    try {
+      const id = req.params.id
+      const cartItem = await CartItem.findByPk(id)
+      await cartItem.update({ quantity: cartItem.quantity - 1 >= 1 ? cartItem.quantity - 1 : 1 })
+      return res.redirect('back')
+    } catch (err) { console.log(err) }
   }
 }
 
--- routes/modules/cart.js
+++ routes/modules/cart.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const cartController = require('../../controllers/cart-controller')
 
+router.post('/:id/add', cartController.addCart)
+router.post('/:id/sub', cartController.subCart)
 router.get('/', cartController.getCart)
 router.post('/', cartController.postCart)
 
