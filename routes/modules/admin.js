0710c9b896e5f9c7fb0a1ec116d0cd99afac7c2a
RobertOu0213 <starshining12345@gmail.com>
2023-11-27 16:37:17 +0800

    feat: add admin middleware

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,25 +3,26 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const passport = require('../../config/passport')
 const upload = require('../../middleware/multer')
+const { authenticatedAdmin } = require('../../middleware/auth')
 
 router.get('/login', adminController.loginPage)
 router.post('/login', passport.authenticate('local', { failureRedirect: '/admin/login', failureFlash: true }), adminController.login)
 router.get('/logout', adminController.logout)
 
-router.get('/products/:id', adminController.getProduct)
-router.get('/products', adminController.getProducts)
+router.get('/products/:id', authenticatedAdmin, adminController.getProduct)
+router.get('/products', authenticatedAdmin, adminController.getProducts)
 router.post('/products', upload.single('image'), adminController.postProducts)
 router.put('/products/:id', upload.single('image'), adminController.putProducts)
 router.delete('/products/:id', adminController.deleteProduct)
 
-router.get('/orders/:id', adminController.getOrder)
-router.get('/orders', adminController.getOrders)
+router.get('/orders/:id', authenticatedAdmin, adminController.getOrder)
+router.get('/orders', authenticatedAdmin, adminController.getOrders)
 router.post('/orders/:id/ship', adminController.shipOrder)
 router.post('/orders/:id/cancel', adminController.cancelOrder)
 router.post('/orders/:id/recover', adminController.recoverOrder)
 
-router.get('/authority', adminController.getAuthority)
+router.get('/authority', authenticatedAdmin, adminController.getAuthority)
 router.post('/authority/:id', adminController.changeAuth)
-router.use('/', (req, res) => res.redirect('/admin/products'))
+router.use('/', authenticatedAdmin, (req, res) => res.redirect('/admin/products'))
 
 module.exports = router
