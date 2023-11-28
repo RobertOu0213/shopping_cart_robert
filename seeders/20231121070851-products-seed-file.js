8fb9c801144f704dcbce795c566ae604c968d339
RobertOu0213 <starshining12345@gmail.com>
2023-11-28 10:19:17 +0800

    fix: faker image

--- seeders/20231121070851-products-seed-file.js
+++ seeders/20231121070851-products-seed-file.js
@@ -8,7 +8,7 @@ module.exports = {
         name: '薯條',
         description: '好吃',
         price: 135,
-        image: '/faker/直條薯條.jpg',
+        image: '/faker/white-fries.jpg',
         created_at: new Date(),
         updated_at: new Date()
       },
@@ -17,7 +17,7 @@ module.exports = {
         name: '洋蔥圈',
         description: '很脆',
         price: 200,
-        image: '/faker/洋蔥圈.jpg',
+        image: '/faker/Onion-rings.jpg',
         created_at: new Date(),
         updated_at: new Date()
       },
@@ -26,16 +26,34 @@ module.exports = {
         name: '薯辦',
         description: '超級好吃',
         price: 100,
-        image: '/faker/紅石薯瓣.jpg',
+        image: '/faker/Potato-Wedges.jpg',
         created_at: new Date(),
         updated_at: new Date()
       },
       {
         id: 4,
-        name: '網狀',
+        name: '網狀薯條',
         description: '超級好吃的',
         price: 150,
-        image: '/faker/紅石網狀.jpg',
+        image: '/faker/Waffle-Fries.jpg',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: 5,
+        name: '調味薯條',
+        description: '鹹香',
+        price: 150,
+        image: '/faker/fries.jpg',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        id: 6,
+        name: '單色起司',
+        description: '濃厚起司',
+        price: 200,
+        image: '/faker/Shredded-Cheese.jpg',
         created_at: new Date(),
         updated_at: new Date()
       }
